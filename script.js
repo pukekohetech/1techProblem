fdce4c6547efe670060d15460cf26407c522bf1f
pukekohetech <33167310+pukekohetech@users.noreply.github.com>
2026-02-06 21:05:55 +1300

    Update minimum percentage required for submission

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ const DEBUG = false; // ← Debug logging off in production
 // ------------------------------------------------------------
 // Requirements
 // ------------------------------------------------------------
-const MIN_PCT_FOR_SUBMIT = 74;
+const MIN_PCT_FOR_SUBMIT = 80;
 // Change this to e.g. 80 if you want 80% or better
 
 function findMostRecentStorageKeyForApp(appId, currentKey) {
