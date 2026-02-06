1f301aec3a44de440ef06a55c366f9874fe751ea
pukekohetech <33167310+pukekohetech@users.noreply.github.com>
2026-02-06 20:29:33 +1300

    Disable input event listener in attachProtection function
    Comment out input event listener for answer fields.

--- script.js
+++ script.js
@@ -1137,7 +1137,7 @@ function clearClipboard() {
 // ------------------------------------------------------------
 function attachProtection() {
   document.querySelectorAll(".answer-field").forEach(f => {
-    f.addEventListener("input", () => saveAnswer(f.id.slice(1)));
+//    f.addEventListener("input", () => saveAnswer(f.id.slice(1)));
  //   f.addEventListener("paste", e => { e.preventDefault(); showToast(PASTE_BLOCKED_MESSAGE, false); clearClipboard(); });
   });
 }
